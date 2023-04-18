e7d890ba67497d1b6abd394ea70c2671ec42513f
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-18 11:52:12 +0900

    Feat: MongoDB 접속 기능 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import cors from 'cors';
 import express from 'express';
 
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const { MONGO_URL } = process.env;
+
 const app = express();
 
 // CORS 에러 방지
@@ -11,5 +16,11 @@ app.use(express.json());
 
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB Connected');
+});
 
 export default app;
