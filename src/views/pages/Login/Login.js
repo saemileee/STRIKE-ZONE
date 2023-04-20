9167bca52e26de0751c6a761dc0395d34410d7b3
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-04-21 03:28:01 +0900

    feat: 로그인 api 연동

--- src/views/pages/Login/Login.js
+++ src/views/pages/Login/Login.js
@@ -9,7 +9,15 @@ const onLoginSubmit = (e) => {
   e.preventDefault();
   const userId = loginId.value;
   const userPassword = loginPassword.value;
-  console.log(userId, userPassword);
+  const userInfo = { email: userId, password: userPassword };
+
+  fetch('/api/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  }).then((response) => console.log(response.json()));
 };
 
 loginForm.addEventListener('submit', onLoginSubmit);
