fa46b6d2a47e668e7e5461da47bf22c41802985c
Saem <saemilee01@gmail.com>
2023-04-20 20:46:25 +0900

    feat : 헤더 GNB 호버 기능 구현

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,13 +1,65 @@
 const DUMMY_USER_DATA = {
-  // userId: 'elice@elice.com',
+  // userId: 'elice@elice.com',d
 };
+const DUMMY_TEAM_CATEGORY_DATA = [
+  { name: 'SSG랜더스', logo: '/assets/img/category/team/SSG.png', url: '#' },
+  {
+    name: '키움히어로즈',
+    logo: '/assets/img/category/team/KIWOOM.png',
+    url: '#',
+  },
+  { name: 'LG트윈스', logo: '/assets/img/category/team/LG.png', url: '#' },
+  { name: 'KT위즈', logo: '/assets/img/category/team/KT.png', url: '#' },
+  { name: 'KIA타이거즈', logo: '/assets/img/category/team/KIA.png', url: '#' },
+  { name: 'NC다이노스', logo: '/assets/img/category/team/NC.png', url: '#' },
+  {
+    name: '삼성라이온즈',
+    logo: '/assets/img/category/team/SAMSUNG.png',
+    url: '#',
+  },
+  {
+    name: '롯데자이언츠',
+    logo: '/assets/img/category/team/LOTTE.png',
+    url: '#',
+  },
+  {
+    name: '두산베어스',
+    logo: '/assets/img/category/team/DOOSAN.png',
+    url: '#',
+  },
+  {
+    name: '한화이글스',
+    logo: '/assets/img/category/team/HANHWA.png',
+    url: '#',
+  },
+];
+const DUMMY_PRODUCT_CATEGORY_DATA = [
+  { name: '유니폼', url: '#' },
+  { name: '모자', url: '#' },
+  { name: '의류', url: '#' },
+  { name: '잡화', url: '#' },
+  { name: '응원용품', url: '#' },
+  { name: '야구용품', url: '#' },
+];
 const CART_AMOUNT = 2;
 
 const headerElement = document.createElement('header');
+const headerWrapper = document.createElement('div');
+headerWrapper.className = 'header-wrapper';
+
 const headerContainer = document.createElement('div');
 headerContainer.className = 'header-container';
-headerContainer.innerHTML = `<h1>MARKET<br/>NAME</h1>
-<ul><li>TEAMS</li><li>PRODUCTS</li></ul>`;
+headerContainer.innerHTML = `<h1>MARKET<br/>NAME</h1>`;
+
+const categoryUlElement = document.createElement('ul');
+
+const teamCategoryLiElement = document.createElement('li');
+teamCategoryLiElement.innerHTML = `TEAMS`;
+
+const productCategoryLiElement = document.createElement('li');
+productCategoryLiElement.innerHTML = `PRODUCTS`;
+
+categoryUlElement.append(teamCategoryLiElement, productCategoryLiElement);
 
 const cartElement = document.createElement('button');
 cartElement.className = 'cart';
@@ -38,6 +90,83 @@ if (!DUMMY_USER_DATA.userId) {
   rightSideElementsContainer.append(myPageElement, logoutElement);
 }
 
-headerElement.append(headerContainer);
-headerContainer.append(rightSideElementsContainer);
+headerElement.append(headerWrapper);
+headerWrapper.append(headerContainer);
+headerContainer.append(categoryUlElement, rightSideElementsContainer);
 document.body.prepend(headerElement);
+
+const createTeamElement = team => {
+  const teamLiElement = document.createElement('li');
+  teamLiElement.innerHTML = `
+    <div>
+      <img src="${team.logo}">
+    </div>
+    <p>${team.name}</p>
+  `;
+  teamLiElement.addEventListener('click', () => {
+    window.location.href = team.url;
+  });
+  return teamLiElement;
+};
+
+const createProductElement = category => {
+  const productLiElement = document.createElement('li');
+  productLiElement.innerHTML = `
+      <p>${category.name}</p>
+  `;
+  productLiElement.addEventListener('click', () => {
+    window.location.href = category.url;
+  });
+  return productLiElement;
+};
+
+const categoryContainerElement = document.createElement('div');
+categoryContainerElement.classList.add('category-container');
+
+const teamsContainerElement = document.createElement('div');
+teamsContainerElement.classList.add('teams-container');
+const teamsUlElement = document.createElement('ul');
+DUMMY_TEAM_CATEGORY_DATA.forEach(team => {
+  const teamElement = createTeamElement(team);
+  teamsUlElement.append(teamElement);
+});
+teamsContainerElement.append(teamsUlElement);
+
+const productsContainerElement = document.createElement('div');
+productsContainerElement.classList.add('products-container');
+const productsUlElement = document.createElement('ul');
+DUMMY_PRODUCT_CATEGORY_DATA.forEach(category => {
+  const productElement = createProductElement(category);
+  productsUlElement.append(productElement);
+});
+productsContainerElement.append(productsUlElement);
+
+categoryContainerElement.append(
+  teamsContainerElement,
+  productsContainerElement
+);
+headerElement.append(categoryContainerElement);
+
+teamsContainerElement.style.display = 'none';
+productsContainerElement.style.display = 'none';
+
+teamCategoryLiElement.addEventListener('mouseover', () => {
+  productsContainerElement.style.display = 'none';
+  teamsContainerElement.style.display = 'block';
+});
+
+productCategoryLiElement.addEventListener('mouseover', () => {
+  teamsContainerElement.style.display = 'none';
+  productsContainerElement.style.display = 'block';
+});
+
+const hideTeamsContainer = () => {
+  teamsContainerElement.style.display = 'none';
+};
+
+teamsContainerElement.addEventListener('mouseleave', hideTeamsContainer);
+
+const hideProductsContainer = () => {
+  productsContainerElement.style.display = 'none';
+};
+productsContainerElement.addEventListener('mouseleave', hideProductsContainer);
