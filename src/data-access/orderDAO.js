bd872f888961412d88dca774eae2b8bce6fc4c90
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-22 00:17:17 +0900

    feat: 새로운 주문을 저장하면 해당 주문 수량만큼 상품 수량에서 차감한다.

--- src/data-access/orderDAO.js
+++ src/data-access/orderDAO.js
@@ -1,15 +1,22 @@
-import { Order } from './model';
+import { Order, Product } from './model';
 
 const orderDAO = {
 
   // 주문 저장하기
   async createOrder(orderInfo) {
-    // 가장 마지막에 등록된 주문을 검색해서 해당 orderId 에서 +1 을 한 값을
-    // 새로 저장할 주문의 orderId 로 사용한다.
+    // 가장 마지막에 등록된 주문의 orderId + 1 해서 새 주문의 orderId 로 사용한다.
     const lastOrder = await Order.find({}).sort({ createdAt: -1 }).limit(1);
     const lastOrderId = lastOrder.length === 0 ? 0 : lastOrder[0].orderId;
     const nextOrderId = lastOrderId + 1;
     await Order.create({ ...orderInfo, orderId: nextOrderId });
+
+    // TODO : 주문을 저장 후, [해당 productId의 상품 수량]에서 [주문 수량]만큼 차감해야 한다.
+    const { productId, quantity: orderedQuantity } = orderInfo;
+
+    const product = await Product.findOne({ productId });
+    const currentProductQuantity = product.quantity;
+
+    await Product.updateOne({ productId }, { quantity: currentProductQuantity - orderedQuantity });
   },
 
   // 모든 주문 정보 조회하기
