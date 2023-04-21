b11a603a9b1ef38c392217a4cc0f9c9877e7f9f0
김민규 <gyepi7676@gmail.com>
2023-04-21 23:51:39 +0900

    feat: 주문 목록 공통 영역 연결 및 리팩토링

--- src/views/pages/OrderComplete/OrderComplete.js
+++ src/views/pages/OrderComplete/OrderComplete.js
@@ -4,33 +4,28 @@ import { $, $createElement } from '/js/utils.js';
 const OrderItem = (target, { team, name, img, amount, price }) => {
   const $item = $createElement('li', 'order-item');
   $item.innerHTML = `
-  <div class="image-container">
-    <img src=${img} />
-  </div>
-  <div class="product-information">
-    <span class="order-product-team">${team}</span>
-    <span class="order-product-title">${name}</span>
-    <span class="order-product-total-amount">${price.toLocaleString()}원</span>
-    <span class="order-product-count">${amount}개</span>
-  </div>
+    <div class="image-container">
+      <img src=${img} />
+    </div>
+    <div class="product-information">
+      <span class="order-product-team">${team}</span>
+      <span class="order-product-title">${name}</span>
+      <span class="order-product-total-amount">${price.toLocaleString()}원</span>
+      <span class="order-product-count">${amount}개</span>
+    </div>
   `;
   target.append($item);
 };
 
 const render = () => {
-  const orderIdByPath = location.pathname.split('/')[2];
-  /*
-
-    주문 정보를 받는 API
-
-  */
+  // const orderIdByPath = location.pathname.split('/')[2];
   const DUMMY = {
     order_id: '2424121',
     orderDate: '2023-04-30',
     priceData: {
       productsPrice: 47000,
-      shipPrice: 3000,
-      totalPrice: 50000,
+      shipPrice: 50000,
+      totalPrice: 3000,
     },
     userData: {
       name: '김민규',
@@ -57,6 +52,65 @@ const render = () => {
       },
     ],
   };
+
+  const $userData = $('.user-data');
+  $userData.innerHTML = `
+    <h3 class="title is-3">주문 내역</h3>
+    <hr>
+    <div class="order-list-container">
+      <div class="order-data order-id-group">
+        <p>주문 번호</p>
+        <em class="order-id">123123</em>
+      </div>
+      <div class="order-data order-address">
+        <p>주문 일자</p>
+        <em class="order-date">123123</em>
+      </div>
+      <hr>
+      <h4 class="title is-4">주문상품</h4>
+      <ul class="order-list">
+      </ul>
+      <hr>
+      <div class="price-box ship">
+        <p>상품 금액</p>
+        <em>47,000</em>
+      </div>
+      <div class="price-box price">
+        <p>배송비</p>
+        <em>3,000</em>
+      </div>
+      <hr>
+      <div class="price-box total-price">
+        <p>총 결제 금액</p>
+        <em>50,000</em>
+      </div>
+    </div>
+    <hr>
+    <div class="order-user-data">
+      <h4 class="title is-3">배송지 정보</h4>
+      <hr>
+      <h4 class="title is-4">받으시는 분</h4>
+      <div class="user-data-box">
+        <div class="user-name">
+          <p>받는 사람</p>
+          <em></em>
+        </div>
+        <div class="user-phone-number">
+          <p>주소</p>
+          <em></em>
+        </div>
+        <div class="user-address">
+          <p>연락처</p>
+          <em></em>
+        </div>
+        <div class="user-requirement">
+          <p>요청사항</p>
+          <em></em>
+        </div>
+      </div>
+    </div>
+  </section>
+  `;
 
   const $orderList = $('.order-list');
   DUMMY.products.forEach(({ team, name, img, amount, price }) => {
