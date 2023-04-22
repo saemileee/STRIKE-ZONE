ab21ee1560e0caeedb530140550da0a9d6ca888a
Saem <saemilee01@gmail.com>
2023-04-22 19:38:36 +0900

    feat : 장바구니 api의 프로덕트id, 선택수량을 기준으로 productsDB에서 정보 끌어와 주문리스트와 결제정보 렌더링하기

--- src/views/pages/Order/Order.js
+++ src/views/pages/Order/Order.js
@@ -1,32 +1,129 @@
 import { $ } from '/js/utils.js';
 import { getCartListSelected, getOrderPrice } from '/js/api/cartAPI.js';
 
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const cartListSelected = getCartListSelected();
 const orderPrice = getOrderPrice();
+const orderListContainer = document.querySelector('.order-list-container');
 
-function renderOrderList() {
-  const orderListContainer = document.querySelector('.order-list-container');
-  cartListSelected.forEach(orderData => {
-    const { amount, id, img, name, price, team, total } = orderData;
-    const orderProduct = document.createElement('div');
-    orderProduct.className = 'order-product';
-    orderProduct.innerHTML = `<div class="image-container">
-    <img src="${img}" />
+async function getData() {
+  const { deliveryCharge } = await fetchData('/api/v1/shippings/default');
+  const DELIVERY_CHARGE = await deliveryCharge;
+
+  let ALL_ORDERS_TOTAL_AMOUNT = 0;
+  for (let cartList of cartListSelected) {
+    const { id: PRODUCT_ID, amount: PURCHASE_QUANTITY } = cartList;
+    const {
+      discountedPrice: DISCOUNTED_PRODUCT_PRICE,
+      name: NAME,
+      teamName: TEAM,
+      price: PRICE,
+      rate: RATE,
+      img: IMAGES,
+    } = await fetchData(`/api/v1/products/${PRODUCT_ID}`);
+
+    const TOTAL_PRODUCT_AMOUNT = DISCOUNTED_PRODUCT_PRICE * PURCHASE_QUANTITY;
+    ALL_ORDERS_TOTAL_AMOUNT += TOTAL_PRODUCT_AMOUNT;
+    renderOrderList(
+      TEAM,
+      NAME,
+      PURCHASE_QUANTITY,
+      RATE,
+      PRICE,
+      DISCOUNTED_PRODUCT_PRICE,
+      TOTAL_PRODUCT_AMOUNT,
+      IMAGES
+    );
+  }
+  const TOTAL_PAYMENT_AMOUNT = ALL_ORDERS_TOTAL_AMOUNT + DELIVERY_CHARGE;
+  renderReceipt(ALL_ORDERS_TOTAL_AMOUNT, DELIVERY_CHARGE, TOTAL_PAYMENT_AMOUNT);
+
+  const checkoutButton = document.querySelector('.check-out-button');
+  checkoutButton.value = `${TOTAL_PAYMENT_AMOUNT.toLocaleString()}원 결제하기`;
+  console.log(checkoutButton);
+}
+
+getData();
+
+function renderOrderList(
+  team,
+  name,
+  quantity,
+  rate,
+  price,
+  discountedPrice,
+  totalProductAmount,
+  images
+) {
+  const orderProduct = document.createElement('div');
+  const img = images[0];
+  orderProduct.className = 'order-product';
+  orderProduct.innerHTML = `<div class="image-container">
+     <img src="${img}" />
   </div>
   <div class="product-information">
     <span class="order-product-team">${team}</span>
     <span class="order-product-title">${name}</span
-    ><span class="order-product-total-amount">${price.toLocaleString()}원</span
-    ><span class="order-product-count">${amount}개</span>
+    ><span class="order-product-rate">${rate}%</span
+    ><span class="order-product-price">${price.toLocaleString()}원</span
+    ><span class="order-product-discounted-price">${discountedPrice.toLocaleString()}원</span
+    ><span class="order-product-count">${quantity}개</span>
+    <span class="order-product-total-amount">${totalProductAmount.toLocaleString()}원</span
+    >
   </div>`;
-    orderListContainer.append(orderProduct);
-  });
+  orderListContainer.append(orderProduct);
+}
 
+function renderReceipt(totalProductAmount, deliveryCharge, totalPaymentAmount) {
   const totalProductAmountElement = document.querySelector(
     '.total-product-amount'
   );
-  totalProductAmountElement.innerHTML = `${orderPrice.toLocaleString()}원`;
+  totalProductAmountElement.innerHTML = `${totalProductAmount.toLocaleString()}원`;
+
+  const shippingChargeElement = document.querySelector('.shipping-charge');
+  shippingChargeElement.innerHTML = `${deliveryCharge.toLocaleString()}원`;
+
+  const totalPaymentAmountElement = document.querySelector('.total-pay-amount');
+  totalPaymentAmountElement.innerHTML = `${totalPaymentAmount.toLocaleString()}원`;
 }
+
+//장바구니 API에서 받아올 때
+// function renderOrderList() {
+//   const orderListContainer = document.querySelector('.order-list-container');
+//   cartListSelected.forEach(orderData => {
+//     const { amount, id, img, name, price, team, total } = orderData;
+//     const orderProduct = document.createElement('div');
+//     orderProduct.className = 'order-product';
+//     orderProduct.innerHTML = `<div class="image-container">
+//     <img src="${img}" />
+//   </div>
+//   <div class="product-information">
+//     <span class="order-product-team">${team}</span>
+//     <span class="order-product-title">${name}</span
+//     ><span class="order-product-total-amount">${price.toLocaleString()}원</span
+//     ><span class="order-product-count">${amount}개</span>
+//   </div>`;
+//     orderListContainer.append(orderProduct);
+//   });
+
+//   const totalProductAmountElement = document.querySelector(
+//     '.total-product-amount'
+//   );
+//   totalProductAmountElement.innerHTML = `${orderPrice.toLocaleString()}원`;
+
+//   const shippingChargeElement = document.querySelector('.shipping-charge');
+
+//   const totalPaymentAmountElement = document.querySelector('.total-pay-amount');
+// }
 
 renderOrderList();
 
