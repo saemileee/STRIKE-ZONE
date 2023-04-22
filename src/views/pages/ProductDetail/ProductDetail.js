8f2c9d2f58304cf57b0c20eb4aa9c7670c30bb60
Saem <saemilee01@gmail.com>
2023-04-22 14:50:34 +0900

    refector : api 연결 및 전체 코드 리팩토링

--- src/views/pages/ProductDetail/ProductDetail.js
+++ src/views/pages/ProductDetail/ProductDetail.js
@@ -13,169 +13,129 @@ function $createImage(src) {
   return $image;
 }
 
-const {
-  name,
+function renderProductImages(productImages) {
+  const $productBasicInformationContainer = $createElement(
+    'div',
+    'product-basic-information-container'
+  );
+  const $productImageViewContainer = document.querySelector(
+    '.product-image-view'
+  );
+
+  const $productImageBigBox = document.querySelector('.product-image-big');
+  $productImageBigBox.innerHTML = `<img src = ${productImages[0]}>`;
+
+  const $productImageSlider = document.querySelector('.product-image-slider');
+  $productImageViewContainer.append($productImageSlider);
+  productImages.forEach(image => {
+    const $productImageSmallBox = $createElement('div', 'product-image-small');
+    const $productImageSmall = $createImage(image);
+    $productImageSmallBox.append($productImageSmall);
+    $productImageSlider.append($productImageSmallBox);
+    $productImageSmallBox.addEventListener('mouseover', () => {
+      $productImageBigBox.innerHTML = `<img src = ${image}>`;
+    });
+  });
+
+  $productImageViewContainer.append($productImageBigBox);
+}
+function readerBasicDescription(
   team,
+  name,
+  shortDescription,
   price,
   rate,
-  detailImages,
-  descriptionImage,
-  descriptionShortText,
-} = DUMMY_DATA;
+  productSellingPrice
+) {
+  const $productTeam = document.querySelector('.product-team');
+  $productTeam.innerHTML = team;
 
-const productID = '734541';
+  const $productTitle = document.querySelector('.product-title');
+  $productTitle.innerHTML = name;
 
-const $productBasicInformationContainer = $createElement(
-  'div',
-  'product-basic-information-container'
-);
-const $productImageViewContainer = $createElement('div', 'product-image-view');
+  const $productShortDescription = document.querySelector(
+    '.product-short-description'
+  );
+  $productShortDescription.innerHTML = shortDescription;
 
-const $productImageBigBox = $createElement('div', 'product-image-big');
-$productImageBigBox.innerHTML = `<img src = ${detailImages[0]}>`;
+  const $productDiscountRate = document.querySelector('.product-discount-rate');
+  $productDiscountRate.innerHTML = `${rate}%`;
 
-const $productImageSlider = $createElement('div', 'product-image-slider');
-$productImageViewContainer.append($productImageSlider);
-detailImages.forEach(image => {
-  const $productImageSmallBox = $createElement('div', 'product-image-small');
-  const $productImageSmall = $createImage(image);
-  $productImageSmallBox.append($productImageSmall);
-  $productImageSlider.append($productImageSmallBox);
-  $productImageSmallBox.addEventListener('mouseover', () => {
-    $productImageBigBox.innerHTML = `<img src = ${image}>`;
-  });
-});
+  const $productPrice = document.querySelector('.product-price');
+  $productPrice.innerHTML = `${price.toLocaleString()}원`;
 
-$productImageViewContainer.append($productImageBigBox);
-
-const $productBasicDescriptionContainer = $createElement(
-  'div',
-  'product-basic-description-container'
-);
-const $productTeam = $createElement('span', 'product-team');
-$productTeam.innerHTML = team;
-
-const $productTitle = $createElement('span', 'product-title');
-$productTitle.innerHTML = name;
-
-const $hr1 = document.createElement('hr');
-
-const $productShortDescription = $createElement(
-  'span',
-  'product-short-description'
-);
-$productShortDescription.innerHTML = descriptionShortText;
-
-const $hr2 = document.createElement('hr');
-
-const $productDiscountRate = $createElement('span', 'product-discount-rate');
-$productDiscountRate.innerHTML = `${rate}%`;
-
-const $productPrice = $createElement('span', 'product-price');
-$productPrice.innerHTML = `${price.toLocaleString()}원`;
-
-const $productSellingPrice = $createElement('span', 'product-selling-price');
-const productSellingPrice = price * ((100 - rate) * 0.01);
-$productSellingPrice.innerHTML = `${productSellingPrice.toLocaleString()}원`;
-
-const $productPriceInformationContainer = $createElement(
-  'div',
-  'product-price-information-container'
-);
-$productPriceInformationContainer.append(
-  $productDiscountRate,
-  $productPrice,
-  $productSellingPrice
-);
-
-const $productCountLabel = document.createElement('label');
-$productCountLabel.setAttribute('for', 'product-count');
-$productCountLabel.innerHTML = '수량';
-
-const $productCountInput = $createElement(
-  'input',
-  'product-count input is-info'
-);
-$productCountInput.setAttribute('id', 'product-count');
-$productCountInput.setAttribute('type', 'number');
-$productCountInput.setAttribute('value', '1');
-$productCountInput.setAttribute('min', '1');
-
-const $productCountContainer = $createElement('div', 'product-count-container');
-$productCountContainer.append($productCountLabel, $productCountInput);
-
-const $hr3 = document.createElement('hr');
-
-const $totalAmountLabel = document.createElement('label');
-$productCountLabel.setAttribute('for', 'product-count');
-$totalAmountLabel.innerHTML = '주문 금액';
-
-let totalAmountValue = productSellingPrice;
-const $totalAmountValue = $createElement('span', 'total-amount');
-$totalAmountValue.innerHTML = `${totalAmountValue.toLocaleString()}원`;
-
-$productCountInput.addEventListener('change', () => {
-  totalAmountValue = productSellingPrice * $productCountInput.value;
+  const $productSellingPrice = document.querySelector('.product-selling-price');
+  $productSellingPrice.innerHTML = `${productSellingPrice.toLocaleString()}원`;
+}
+function renderPrice(price, quantity, productSellingPrice) {
+  let totalAmountValue = price;
+  const $productCountInput = document.querySelector('.product-count');
+  $productCountInput.setAttribute('max', `${quantity}`);
+  const $totalAmountValue = document.querySelector('.total-amount');
   $totalAmountValue.innerHTML = `${totalAmountValue.toLocaleString()}원`;
-});
+  $productCountInput.addEventListener('change', () => {
+    totalAmountValue = productSellingPrice * $productCountInput.value;
+    $totalAmountValue.innerHTML = `${totalAmountValue.toLocaleString()}원`;
+  });
+}
+function renderBuyButtons(productId) {
+  const $cartButton = document.querySelector('.cart-button');
+  $cartButton.addEventListener('click', () => {
+    const $productCountInput = document.querySelector('.product-count');
+    addItemCart(productId, $productCountInput.value);
+    if (
+      confirm(
+        '해당 상품이 장바구니에 추가되었습니다. 바로 장바구니를 확인하시겠습니까?'
+      ) === true
+    ) {
+      window.location.href = '/cart';
+    }
+  });
+}
 
-const $totalAmountContainer = $createElement('div', 'total-amount-container');
-$totalAmountContainer.append($totalAmountLabel, $totalAmountValue);
+function renderProductDetailDescription(productDetailDescription) {
+  const $detailDescription = document.querySelector(
+    '.product-detail-description'
+  );
+  $detailDescription.innerHTML = `${productDetailDescription}`;
+}
 
-const $cartButton = $createElement('button', 'cart-button button');
-$cartButton.innerHTML = '장바구니';
-$cartButton.addEventListener('click', () => {
-  addItemCart(productID, $productCountInput.value);
-  if (
-    confirm(
-      '해당 상품이 장바구니에 추가되었습니다. 바로 장바구니를 확인하시겠습니까?'
-    ) === true
-  ) {
-    window.location.href = '/cart';
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
   }
-});
+}
 
-const $orderButton = $createElement('button', 'order-button button is-info');
-$orderButton.innerHTML = '바로구매';
+async function getProductData() {
+  const productData = await fetchData('/api/v1/products/1');
+  const {
+    productId,
+    teamName,
+    name,
+    price,
+    rate,
+    quantity,
+    img,
+    shortDescription,
+    detailDescription,
+  } = productData;
+  const productSellingPrice = price * ((100 - rate) * 0.01);
+  renderProductImages(img);
+  readerBasicDescription(
+    teamName,
+    name,
+    shortDescription,
+    price,
+    rate,
+    productSellingPrice
+  );
+  renderPrice(price, quantity, productSellingPrice);
+  renderBuyButtons(productId);
+  renderProductDetailDescription(detailDescription);
+}
 
-const $buttonsContainer = $createElement('div', 'buttons-container');
-$buttonsContainer.append($cartButton, $orderButton);
-
-$productBasicDescriptionContainer.append(
-  $productTeam,
-  $productTitle,
-  $hr1,
-  $productShortDescription,
-  $hr2,
-  $productPriceInformationContainer,
-  $hr3,
-  $productCountContainer,
-  $totalAmountContainer,
-  $buttonsContainer
-);
-
-const $descriptionHeader = $createElement('li', 'is-active');
-$descriptionHeader.innerHTML = '<a>상품정보</a>';
-
-const $tabsUl = $createElement('ul', '_');
-$tabsUl.append($descriptionHeader);
-const $tabs = $createElement('div', 'tabs is-large');
-$tabs.append($tabsUl);
-
-const $descriptionImage = $createImage(descriptionImage);
-
-const $detailDescriptionContainer = $createElement(
-  'div',
-  'detail-description-container'
-);
-$detailDescriptionContainer.append($tabs, $descriptionImage);
-
-const $productDetailWrapper = document.querySelector('.product-detail-wrapper');
-$productDetailWrapper.append(
-  $productBasicInformationContainer,
-  $detailDescriptionContainer
-);
-$productBasicInformationContainer.append(
-  $productImageViewContainer,
-  $productBasicDescriptionContainer
-);
+getProductData();
