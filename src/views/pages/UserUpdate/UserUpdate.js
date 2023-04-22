1e9eeff37b767dc28c53cc7add49b55aab60dd91
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-04-22 17:06:43 +0900

    feat: 유저 정보 수정 시 로그인 api 연동

--- src/views/pages/UserUpdate/UserUpdate.js
+++ src/views/pages/UserUpdate/UserUpdate.js
@@ -13,7 +13,6 @@ function getUser() {
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
-// getUser();
 
 const $userData = $('.user-data');
 
@@ -255,188 +254,219 @@ const checkPasswordForm = $createElement('form', 'check-login-form');
 checkPasswordForm.innerHTML = `
 <section class="check-login-wrapper">
   <h3 class="title is-3">로그인</h3>
-  <div class="login-form">
-    <div class="login-input">
-      <input
-        type="text"
-        placeholder="이메일"
-        class="input check-email"
-        id="loginId"
-        required
-      />
-      <input
-        type="password"
-        placeholder="비밀번호"
-        class="input check-password"
-        id="loginPassword"
-        required
-      />
-    </div>
-    <button type="submit" class="button is-info" id="login">로그인</button>
+  <div class="login-form-wrapper">
+    <div class="login-form">
+      <div class="login-input">
+        <input
+          type="text"
+          placeholder="이메일"
+          class="input check-email"
+          id="loginId"
+          required
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          class="input check-password"
+          id="loginPassword"
+          required
+        />
+      </div>
+      <button type="submit" class="button is-info" id="login">로그인</button>
+    </div>    
   </div>
-    </section>`;
+</section>
+`;
 
 $userData.append(checkPasswordForm);
+
+function showUpdateForm() {
+  checkPasswordForm.remove();
+
+  // getUser();
+
+  $userData.append($form);
+
+  const updateForm = $('.update-form');
+  const newUserEmail = $('#email');
+  const newUserPassword = $('#password');
+  const newUserPasswordVerify = $('#passwordVerify');
+  const newUserPhoneNumber = $('#phoneNumber');
+  const findAddress = document.querySelectorAll('.address');
+  const teams = document.getElementsByName('team');
+
+  function checkValidation(target) {
+    const regex = {
+      email:
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+      password: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/,
+    };
+    if (!target.value.match(regex[target.id])) return false;
+    return true;
+  }
+
+  function isValid(event) {
+    if (!checkValidation(event.target)) {
+      event.target.classList.add('is-danger');
+      const warning = $(`.${event.target.id}-warning`);
+      warning.style.display = '';
+    } else {
+      event.target.classList.remove('is-danger');
+      const warning = $(`.${event.target.id}-warning`);
+      warning.style.display = 'none';
+    }
+  }
+
+  function passwordVerify() {
+    const warning = $('.password-verify-warning');
+    if (
+      newUserPassword.value !== newUserPasswordVerify.value ||
+      newUserPasswordVerify.value === ''
+    ) {
+      newUserPasswordVerify.classList.add('is-danger');
+      warning.style.display = '';
+      return false;
+    }
+    newUserPasswordVerify.classList.remove('is-danger');
+    warning.style.display = 'none';
+    return true;
+  }
+
+  function userInfoComplete() {
+    if (!checkValidation(newUserEmail)) {
+      alert('이메일 형식이 올바르지 않습니다.');
+      return false;
+    }
+    if (!checkValidation(newUserPassword)) {
+      alert('비밀번호 형식이 올바르지 않습니다.');
+      return false;
+    }
+    if (!passwordVerify()) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return false;
+    }
+    return true;
+  }
+
+  function autoHyphen() {
+    newUserPhoneNumber.value = newUserPhoneNumber.value
+      .replace(/[^0-9]/g, '')
+      .replace(/^(\d{3,4})(\d{4})$/, '$1-$2');
+  }
+
+  function getPhoneNumber() {
+    const firstNumber = $('#firstPhoneNumber');
+    return `${firstNumber.options[firstNumber.selectedIndex].text}-${
+      newUserPhoneNumber.value
+    }`;
+  }
+
+  function searchZipcode() {
+    new daum.Postcode({
+      oncomplete(data) {
+        const roadAddr = data.roadAddress;
+        document.getElementById('postCode').value = data.zonecode;
+        document.getElementById('roughAddress').value = roadAddr;
+      },
+    }).open();
+  }
+
+  function selectTeam() {
+    for (let i = 0; i < teams.length; i++) {
+      if (teams[i].checked) {
+        teams[i].parentNode.classList.add('is-info');
+      } else {
+        teams[i].parentNode.classList.remove('is-info');
+      }
+    }
+  }
+
+  function getCheerTeam() {
+    for (let i = 0; i < teams.length; i++) {
+      if (teams[i].checked) return teams[i].value;
+    }
+  }
+
+  function onUpdateSubmit(e) {
+    e.preventDefault();
+    if (userInfoComplete()) {
+      const newUser = {};
+      const userInfoKey = [
+        'email',
+        'password',
+        'koreanName',
+        'phoneNumber',
+        'postCode',
+        'roughAddress',
+        'detailAddress',
+        'cheerTeam',
+      ];
+      for (let i = 0; i < userInfoKey.length; i++) {
+        const userInfo = $(`#${userInfoKey[i]}`);
+        if (userInfoKey[i] === 'phoneNumber') {
+          newUser[userInfoKey[i]] = getPhoneNumber();
+        } else if (userInfoKey[i] === 'cheerTeam') {
+          newUser[userInfoKey[i]] = getCheerTeam();
+        } else {
+          newUser[userInfoKey[i]] = userInfo.value;
+        }
+      }
+
+      fetch('/api/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      }).then((user) => console.log(user));
+    }
+  }
+
+  updateForm.addEventListener('submit', onUpdateSubmit);
+  newUserEmail.addEventListener('blur', isValid);
+  newUserPassword.addEventListener('blur', isValid);
+  newUserPassword.addEventListener('blur', passwordVerify);
+  newUserPasswordVerify.addEventListener('blur', passwordVerify);
+  newUserPhoneNumber.addEventListener('input', autoHyphen);
+  for (let i = 0; i < 3; i++) {
+    findAddress[i].addEventListener('click', searchZipcode);
+  }
+  for (let i = 0; i < teams.length; i++) {
+    teams[i].addEventListener('click', selectTeam);
+  }
+}
+
+function fillUserInfo() {}
 
 const checkForm = $('.check-login-form');
 checkForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const ckeckEmail = $('.check-email').value;
   const checkPassword = $('.check-password').value;
+  const userInfo = { email: ckeckEmail, password: checkPassword };
+  const { token } = JSON.parse(localStorage.getItem('user'));
 
-  if (ckeckEmail === 'qwer' && checkPassword === '1111') {
-    checkPasswordForm.remove();
-
-    // getUser();
-
-    $userData.append($form);
-
-    const updateForm = $('.update-form');
-    const newUserEmail = $('#email');
-    const newUserPassword = $('#password');
-    const newUserPasswordVerify = $('#passwordVerify');
-    const newUserPhoneNumber = $('#phoneNumber');
-    const findAddress = document.querySelectorAll('.address');
-    const teams = document.getElementsByName('team');
-
-    function checkValidation(target) {
-      const regex = {
-        email:
-          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-        password: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/,
-      };
-      if (!target.value.match(regex[target.id])) return false;
-      return true;
-    }
-
-    function isValid(event) {
-      if (!checkValidation(event.target)) {
-        event.target.classList.add('is-danger');
-        const warning = $(`.${event.target.id}-warning`);
-        warning.style.display = '';
-      } else {
-        event.target.classList.remove('is-danger');
-        const warning = $(`.${event.target.id}-warning`);
-        warning.style.display = 'none';
+  fetch('/api/v1/auth/check', {
+    method: 'POST',
+    headers: {
+      token,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      getUser();
+      showUpdateForm();
+      fillUserInfo();
+    })
+    .catch(() => {
+      const isloginWarning = $('.login-warning');
+      if (isloginWarning === null) {
+        const loginFormWrapper = $('.login-form-wrapper');
+        const loginWarning = $createElement('div', 'login-warning');
+        loginWarning.innerText = '아이디 또는 비밀번호가 올바르지 않습니다.';
+        loginFormWrapper.appendChild(loginWarning);
       }
-    }
-
-    function passwordVerify() {
-      const warning = $('.password-verify-warning');
-      if (
-        newUserPassword.value !== newUserPasswordVerify.value ||
-        newUserPasswordVerify.value === ''
-      ) {
-        newUserPasswordVerify.classList.add('is-danger');
-        warning.style.display = '';
-        return false;
-      }
-      newUserPasswordVerify.classList.remove('is-danger');
-      warning.style.display = 'none';
-      return true;
-    }
-
-    function userInfoComplete() {
-      if (!checkValidation(newUserEmail)) {
-        alert('이메일 형식이 올바르지 않습니다.');
-        return false;
-      }
-      if (!checkValidation(newUserPassword)) {
-        alert('비밀번호 형식이 올바르지 않습니다.');
-        return false;
-      }
-      if (!passwordVerify()) {
-        alert('비밀번호가 일치하지 않습니다.');
-        return false;
-      }
-      return true;
-    }
-
-    function autoHyphen() {
-      newUserPhoneNumber.value = newUserPhoneNumber.value
-        .replace(/[^0-9]/g, '')
-        .replace(/^(\d{3,4})(\d{4})$/, '$1-$2');
-    }
-
-    function getPhoneNumber() {
-      const firstNumber = $('#firstPhoneNumber');
-      return `${firstNumber.options[firstNumber.selectedIndex].text}-${
-        newUserPhoneNumber.value
-      }`;
-    }
-
-    function searchZipcode() {
-      new daum.Postcode({
-        oncomplete(data) {
-          const roadAddr = data.roadAddress;
-          document.getElementById('postCode').value = data.zonecode;
-          document.getElementById('roughAddress').value = roadAddr;
-        },
-      }).open();
-    }
-
-    function selectTeam() {
-      for (let i = 0; i < teams.length; i++) {
-        if (teams[i].checked) {
-          teams[i].parentNode.classList.add('is-info');
-        } else {
-          teams[i].parentNode.classList.remove('is-info');
-        }
-      }
-    }
-
-    function getCheerTeam() {
-      for (let i = 0; i < teams.length; i++) {
-        if (teams[i].checked) return teams[i].value;
-      }
-    }
-
-    function onUpdateSubmit(e) {
-      e.preventDefault();
-      if (userInfoComplete()) {
-        const newUser = {};
-        const userInfoKey = [
-          'email',
-          'password',
-          'koreanName',
-          'phoneNumber',
-          'postCode',
-          'roughAddress',
-          'detailAddress',
-          'cheerTeam',
-        ];
-        for (let i = 0; i < userInfoKey.length; i++) {
-          const userInfo = $(`#${userInfoKey[i]}`);
-          if (userInfoKey[i] === 'phoneNumber') {
-            newUser[userInfoKey[i]] = getPhoneNumber();
-          } else if (userInfoKey[i] === 'cheerTeam') {
-            newUser[userInfoKey[i]] = getCheerTeam();
-          } else {
-            newUser[userInfoKey[i]] = userInfo.value;
-          }
-        }
-
-        fetch('/api/v1/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newUser),
-        }).then((user) => console.log(user));
-      }
-    }
-
-    updateForm.addEventListener('submit', onUpdateSubmit);
-    newUserEmail.addEventListener('blur', isValid);
-    newUserPassword.addEventListener('blur', isValid);
-    newUserPassword.addEventListener('blur', passwordVerify);
-    newUserPasswordVerify.addEventListener('blur', passwordVerify);
-    newUserPhoneNumber.addEventListener('input', autoHyphen);
-    for (let i = 0; i < 3; i++) {
-      findAddress[i].addEventListener('click', searchZipcode);
-    }
-    for (let i = 0; i < teams.length; i++) {
-      teams[i].addEventListener('click', selectTeam);
-    }
-  }
+    });
 });
