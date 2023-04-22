21115916bee7555c275b700b9b764042f56d0db1
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-23 03:50:46 +0900

    fix: 상품 카테고리명의 enum 을 제거한다. - 좀 더 자유로운 카테고리 이름을 사용할 수 있도록 한다.

--- src/data-access/schemas/categorySchema.js
+++ src/data-access/schemas/categorySchema.js
@@ -2,30 +2,12 @@ import { Schema } from 'mongoose';
 
 const CategorySchema = new Schema(
   {
-    teamId: {
-      type: String,
-      required: true,
-    },
-    teamName: {
-      type: String,
-      required: true,
-    },
-    teamDescription: {
-      type: String,
-      required: true,
-    },
-    categoryId: {
-      type: String,
-      required: true,
-    },
-    categoryName: {
-      type: String,
-      enum: ['유니폼', '모자', '의류', '잡화', '응원용품', '야구용품'],
-      required: true,
-    },
-    categoryDescription: {
-      type: String,
-    },
+    teamId: { type: String, required: true },
+    teamName: { type: String, required: true },
+    teamDescription: { type: String, required: true },
+    categoryId: { type: String, required: true },
+    categoryName: { type: String, required: true },
+    categoryDescription: { type: String },
   },
   {
     collection: 'categories',
