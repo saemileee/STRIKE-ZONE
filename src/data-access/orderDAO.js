5a5f962cb7693ea82444a25a9f8f756038790ea8
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-22 14:24:45 +0900

    refactor: orderId 생성, 상품 수량 업데이트 기능 등을 함수로 분리한다.

--- src/data-access/orderDAO.js
+++ src/data-access/orderDAO.js
@@ -2,13 +2,59 @@ import { Order, Product } from './model';
 
 const orderDAO = {
 
-  // 총 결제액 계산 함수
+  async createOrder(orderInfo) {
+    // 총 상품 금액과 총 주문 금액 계산하기
+    const { productsPayment, totalPayment } = this.calculatePayments(orderInfo);
+
+    // orderId 계산하기
+    const orderId = await this.createOrderId();
+
+    // 주문 등록하기
+    await Order.create({
+      ...orderInfo, orderId, productsPayment, totalPayment,
+    });
+
+    // 주문을 저장 후, [해당 productId의 상품 수량]에서 [주문 수량]만큼 차감해야 한다.
+    await this.updateProductQuantityByOrder(orderInfo);
+
+    // 완료된 주문 정보를 클라이언트에 전달하기
+    const createdOrder = await Order.findOne({ orderId });
+    return createdOrder;
+  },
+
+  async getAllOrders() {
+    const orders = await Order.find({});
+
+    return orders;
+  },
+
+  async getOrdersByUserId(ordererEmail) {
+    const orders = await Order.find({ 'orderer.email': ordererEmail });
+
+    return orders;
+  },
+
+  async getOrderByOrderId(orderId) {
+    const order = await Order.findOne({ orderId });
+
+    return order;
+  },
+
+  async createOrderId() {
+    // 가장 마지막에 등록된 주문의 orderId + 1 해서 새 주문의 orderId 로 사용한다.
+    const lastOrder = await Order.find({}).sort({ createdAt: -1 }).limit(1);
+    const lastOrderId = lastOrder.length === 0 ? 0 : lastOrder[0].orderId;
+    const nextOrderId = lastOrderId + 1;
+
+    return nextOrderId;
+  },
+
   calculatePayments(orderInfo) {
     const initialValue = 0;
     const { products, deliveryCharge } = orderInfo;
 
     const productsPayment = products.reduce(
-      (acc, product) => acc + (product.quantity * product.price),
+      (accumulator, product) => accumulator + (product.quantity * product.price),
       initialValue,
     );
 
@@ -16,56 +62,31 @@ const orderDAO = {
     return { productsPayment, totalPayment };
   },
 
-  // 주문 저장하기
-  async createOrder(orderInfo) {
-    // 가장 마지막에 등록된 주문의 orderId + 1 해서 새 주문의 orderId 로 사용한다.
-    const lastOrder = await Order.find({}).sort({ createdAt: -1 }).limit(1);
-    const lastOrderId = lastOrder.length === 0 ? 0 : lastOrder[0].orderId;
-    const nextOrderId = lastOrderId + 1;
+  async updateProductQuantityByOrder(orderInfo) {
+    const { products } = orderInfo;
 
-    // 총 상품 금액과 총 주문 금액 계산하기
-    const { productsPayment, totalPayment } = this.calculatePayments(orderInfo);
+    // 주문한 상품마다 productId, quantity 를 확인한다.
+    const orderedProductsIdAndQuantity = products.map(
+      ({ productId, quantity }) => (
+        { orderedProductId: productId, orderedProductQuantity: quantity }
+      ),
+    );
 
-    // 주문 등록하기
-    await Order.create({
-      ...orderInfo, orderId: nextOrderId, productsPayment, totalPayment,
-    });
+    orderedProductsIdAndQuantity.forEach(
+      async ({ orderedProductId, orderedProductQuantity }) => {
+      // 주문한 상품 정보를 조회해서 재고 확인하기
+        const product = await Product.findOne({ productId: orderedProductId });
 
-    // TODO : 주문을 저장 후, [해당 productId의 상품 수량]에서 [주문 수량]만큼 차감해야 한다.
-    // !!따로 함수로 분리하기!!
-    // const { productId, quantity: orderedQuantity } = orderInfo;
+        // 수정될 상품 수량 계산하기
+        const quantityToBeUpdated = product.quantity - orderedProductQuantity;
 
-    // const product = await Product.findOne({ productId });
-    // const currentProductQuantity = product.quantity;
-
-    // await Product.updateOne({ productId }, { quantity: currentProductQuantity - orderedQuantity });
-
-    // TODO : 완료된 주문 정보를 다시 전달
-    const createdOrder = await Order.findOne({ orderId: nextOrderId });
-    return createdOrder;
+        await Product.updateOne(
+          { productId: orderedProductId },
+          { quantity: quantityToBeUpdated },
+        );
+      },
+    );
   },
-
-  // 모든 주문 정보 조회하기
-  async getAllOrders() {
-    const orders = await Order.find({});
-
-    return orders;
-  },
-
-  // 특정 유저의 모든 주문 정보 조회하기
-  async getOrdersByUserId(ordererEmail) {
-    const orders = await Order.find({ 'orderer.email': ordererEmail });
-
-    return orders;
-  },
-
-  // 특정 orderId 에 해당하는 주문 정보 조회하기
-  async getOrderByOrderId(orderId) {
-    const order = await Order.findOne({ orderId });
-
-    return order;
-  },
-
 };
 
 export { orderDAO };
