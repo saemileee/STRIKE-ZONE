11e78fd3b0a4e9bb34b4f4612e724e8e3954eac4
Saem <saemilee01@gmail.com>
2023-04-23 23:58:37 +0900

    fix : 메인 배너 스타일 수정

--- src/views/pages/Main/Main.js
+++ src/views/pages/Main/Main.js
@@ -9,28 +9,28 @@ const DUMMY_DATA = [
   {
     mainText: `NO LIMITS<br/>AMAZING LANDERS`,
     subText: 'SSG 랜더스',
-    teamPageURL: '#',
+    teamPageURL: '/products/?team=ssg-landers&category=all',
     mainImage: '/assets/img/main/banner/ssg-landers.png',
     backgroundText: `SSG LANDERS`,
   },
   {
     mainText: `We go up,<br/>Win the championship`,
     subText: '키움 히어로즈',
-    teamPageURL: '#',
+    teamPageURL: '/products/?team=kiwoom-heroes&category=all',
     mainImage: '/assets/img/main/banner/kiwoom.png',
     backgroundText: `KIWOOM HEROES`,
   },
   {
     mainText: `압도하라 Always<br/>KIA TIGERS`,
     subText: '기아 타이거즈',
-    teamPageURL: '#',
+    teamPageURL: '/products/?team=kia-tigers&category=all',
     mainImage: '/assets/img/main/banner/kia.png',
     backgroundText: `KIA TIGERS`,
   },
   {
     mainText: `Win or Wow`,
     subText: '삼성 라이온즈',
-    teamPageURL: '#',
+    teamPageURL: '/products/?team=samsung-lions&category=all',
     mainImage: '/assets/img/main/banner/samsung.png',
     backgroundText: `SAMSUNG LIONS`,
   },
@@ -45,95 +45,103 @@ const nextArrowButtonElement = document.createElement('button');
 nextArrowButtonElement.innerHTML = `<i class="fa fa-chevron-right" style="font-size: 36px"></i>`;
 nextArrowButtonElement.className = 'next-button';
 
-let currentBannerIndex = 0;
+function renderSlideBanner() {
+  const SLIDE_AUTO_PLAY_TIME = 5000;
+  let slideAutoPlayTimer = setInterval(
+    () => nextArrowButtonClickHandler(),
+    SLIDE_AUTO_PLAY_TIME
+  );
+  let currentBannerIndex = 0;
 
-function renderBannerContents() {
-  const { mainText, subText, teamPageURL, mainImage, backgroundText } =
-    DUMMY_DATA[currentBannerIndex];
+  function renderBannerContents() {
+    const { mainText, subText, teamPageURL, mainImage, backgroundText } =
+      DUMMY_DATA[currentBannerIndex];
 
-  const bannerContentLeftElement = document.createElement('div');
-  bannerContentLeftElement.className = 'banner-content-left';
-  bannerContentLeftElement.innerHTML = `<span class="sub-text">${subText}</span><span class="main-text">${mainText}</span>`;
+    const bannerContentLeftElement = document.createElement('div');
+    bannerContentLeftElement.className = 'banner-content-left';
+    bannerContentLeftElement.innerHTML = `<span class="sub-text">${subText}</span><span class="main-text">${mainText}</span>`;
 
-  const CTAButtonElement = document.createElement('button');
-  CTAButtonElement.className = 'CTA-button';
-  CTAButtonElement.innerHTML = 'SHOP NOW';
-  CTAButtonElement.addEventListener('click', () => {
-    window.location.href = teamPageURL;
+    const CTAButtonElement = document.createElement('button');
+    CTAButtonElement.className = 'CTA-button';
+    CTAButtonElement.innerHTML = 'SHOP NOW';
+    CTAButtonElement.addEventListener('click', () => {
+      window.location.href = teamPageURL;
+    });
+
+    bannerContentLeftElement.append(CTAButtonElement);
+
+    const bannerImageElement = document.createElement('div');
+    bannerImageElement.className = 'main-image';
+    bannerImageElement.innerHTML = `<div class="main-image"><img src=${mainImage} /></div>`;
+
+    const backgroundTextElement = document.createElement('div');
+    backgroundTextElement.className = 'background-text';
+
+    function backgroundTextElements(textIndex) {
+      return backgroundText
+        .split(' ')
+        [textIndex].split('')
+        .map((character, index) => {
+          const TEXT_ANIMATION_DURATION = 0.5 + index / 10;
+          return `<span style="animation-duration:${TEXT_ANIMATION_DURATION}s">${character}</span>`;
+        })
+        .join('');
+    }
+
+    backgroundTextElement.innerHTML = `${backgroundTextElements(
+      0
+    )}<br /> ${backgroundTextElements(1)}`;
+
+    bannerContentElement.innerHTML = '';
+    bannerContentElement.append(
+      bannerContentLeftElement,
+      bannerImageElement,
+      backgroundTextElement
+    );
+  }
+  renderBannerContents();
+
+  function prevArrowButtonClickHandler() {
+    if (currentBannerIndex === 0) {
+      currentBannerIndex = DUMMY_DATA.length - 1;
+    } else {
+      currentBannerIndex -= 1;
+    }
+    renderBannerContents();
+  }
+
+  function nextArrowButtonClickHandler() {
+    if (currentBannerIndex === DUMMY_DATA.length - 1) {
+      currentBannerIndex = 0;
+    } else {
+      currentBannerIndex += 1;
+    }
+    renderBannerContents();
+  }
+
+  mainBannerElement.addEventListener('mouseenter', () => {
+    clearInterval(slideAutoPlayTimer);
   });
 
-  bannerContentLeftElement.append(CTAButtonElement);
+  mainBannerElement.addEventListener('mouseleave', () => {
+    slideAutoPlayTimer = setInterval(() => {
+      nextArrowButtonClickHandler();
+    }, SLIDE_AUTO_PLAY_TIME);
+  });
 
-  const bannerImageElement = document.createElement('div');
-  bannerImageElement.className = 'main-image';
-  bannerImageElement.innerHTML = `<div class="main-image"><img src=${mainImage} /></div>`;
+  prevArrowButtonElement.addEventListener('click', () => {
+    clearInterval(slideAutoPlayTimer);
+    prevArrowButtonClickHandler();
+  });
+  nextArrowButtonElement.addEventListener('click', () => {
+    clearInterval(slideAutoPlayTimer);
+    nextArrowButtonClickHandler();
+  });
 
-  const backgroundTextElement = document.createElement('div');
-  backgroundTextElement.className = 'background-text';
-
-  function backgroundTextElements(textIndex) {
-    return backgroundText
-      .split(' ')
-      [textIndex].split('')
-      .map((character, index) => {
-        const TEXT_ANIMATION_DURATION = 0.5 + index / 10;
-        return `<span style="animation-duration:${TEXT_ANIMATION_DURATION}s">${character}</span>`;
-      })
-      .join('');
-  }
-
-  backgroundTextElement.innerHTML = `${backgroundTextElements(
-    0
-  )}<br /> ${backgroundTextElements(1)}`;
-
-  bannerContentElement.innerHTML = '';
-  bannerContentElement.append(
-    bannerContentLeftElement,
-    bannerImageElement,
-    backgroundTextElement
-  );
+  arrowButtonsElement.append(prevArrowButtonElement, nextArrowButtonElement);
+  mainBannerElement.append(arrowButtonsElement);
 }
-renderBannerContents();
-
-function prevArrowButtonClickHandler() {
-  if (currentBannerIndex === 0) {
-    currentBannerIndex = DUMMY_DATA.length - 1;
-  } else {
-    currentBannerIndex -= 1;
-  }
-  renderBannerContents();
-}
-
-function nextArrowButtonClickHandler() {
-  if (currentBannerIndex === DUMMY_DATA.length - 1) {
-    currentBannerIndex = 0;
-  } else {
-    currentBannerIndex += 1;
-  }
-  renderBannerContents();
-}
-
-const SLIDE_AUTO_PLAY_TIME = 5000;
-let slideAutoPlayTimer = setInterval(
-  () => nextArrowButtonClickHandler(),
-  SLIDE_AUTO_PLAY_TIME
-);
-
-mainBannerElement.addEventListener('mouseover', () => {
-  clearInterval(slideAutoPlayTimer);
-});
-
-mainBannerElement.addEventListener('mouseout', () => {
-  slideAutoPlayTimer = setInterval(() => {
-    nextArrowButtonClickHandler(); // 다음 슬라이드를 보여주는 함수
-  }, SLIDE_AUTO_PLAY_TIME);
-});
-
-prevArrowButtonElement.addEventListener('click', prevArrowButtonClickHandler);
-nextArrowButtonElement.addEventListener('click', nextArrowButtonClickHandler);
-
-arrowButtonsElement.append(prevArrowButtonElement, nextArrowButtonElement);
-mainBannerElement.append(arrowButtonsElement);
+renderSlideBanner();
 
 //상품 콘텐츠
 const products = await fetchData('/products');
@@ -154,8 +162,8 @@ function renderRelatedProducts() {
 
   function paintDocuments() {
     const productsContents = document.createElement('section');
-    productsContents.className = 'product-list-contianer related';
-    productsContents.innerHTML = `<h3 class="title is-3">${koreanName}님이 응원하는 ${cheerTeam.teamName}에서 새로 나왔어요!</h3>
+    productsContents.className = 'product-list-container related';
+    productsContents.innerHTML = `<h3 class="title">${koreanName}님이 응원하는 ${cheerTeam.teamName}에서 새로 나왔어요!</h3>
   <div class="products"></div>`;
     productsContainer.prepend(productsContents);
 
@@ -199,7 +207,7 @@ function renderDiscountProducts() {
 
   for (let i = 0; i < 4; i++) {
     renderProducts(
-      document.querySelector('.popular .products'),
+      document.querySelector('.discounted .products'),
       newProductsSorting[i]
     );
   }
