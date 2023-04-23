acfb65657b41ca8f8bdc58156e1988c5989bae11
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-23 23:17:01 +0900

    refactor: 상품 추가 API 리팩토링  - DAO 에서 처리하던 로직을 Service 로 옮기기

--- src/data-access/productDAO.js
+++ src/data-access/productDAO.js
@@ -28,34 +28,17 @@ const productDAO = {
   },
 
   // 상품 추가 (관리자)
-  async createProduct(categoryId, productInfo) {
+  async createProduct(productInfo) {
     // 가장 마지막에 등록한 상품을 검색해서 해당 productId 에서 +1 을 한 값을
     // 새로 등록할 상품의 productId 로 사용한다.
     const lastProduct = await Product.find({}).sort({ createdAt: -1 }).limit(1);
     const lastProductId = lastProduct.length === 0 ? 0 : lastProduct[0].productId;
     const nextProductId = lastProductId + 1;
 
-    const category = await Category.findOne({ categoryId });
-
-    const {
-      teamId, teamName, teamDescription, categoryName,
-    } = category;
-
-    // 할인율을 적용한 상품 가격 계산하기
-    const discountedPrice = this.calculateDiscountedPrice(productInfo);
-
-    const createNewProduct = await Product.create({
-      teamId,
-      teamName,
-      teamDescription,
-      categoryId,
-      categoryName,
-      productId: nextProductId,
-      discountedPrice,
+    await Product.create({
       ...productInfo,
+      productId: nextProductId,
     });
-
-    return createNewProduct;
   },
 
   // 상품 수정
@@ -68,14 +51,6 @@ const productDAO = {
   // 상품 삭제
   async deleteProductByProductId(productId) {
     await Product.deleteOne({ productId });
-  },
-
-  // 할인된 가격을 구하는 함수
-  calculateDiscountedPrice(productInfo) {
-    const { price, rate } = productInfo;
-    const discountedPrice = rate === 0 ? price : price - (price * (rate / 100));
-
-    return discountedPrice;
   },
 };
 
