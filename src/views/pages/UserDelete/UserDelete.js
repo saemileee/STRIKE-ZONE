a836df136f3f4f28cee0905c5e7f84bc1d7b8da8
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-04-24 12:01:07 +0900

    feat: 사용자 정보 수정, 회원 탈퇴 시 로그인 여부 확인

--- src/views/pages/UserDelete/UserDelete.js
+++ src/views/pages/UserDelete/UserDelete.js
@@ -1,50 +1,63 @@
 import { $, $createElement, getCookie, deleteCookie } from '/js/utils.js';
+import { isLogin } from '/js/api/authAPI.js';
 
-const $userData = $('.user-data');
-const deleteConfirm = $createElement('div', 'delete-confirm');
-deleteConfirm.innerHTML = `
+const render = async () => {
+  let loginEmail;
+  try {
+    loginEmail = await isLogin();
+    if (!loginEmail) {
+      alert('회원 전용 페이지입니다!');
+      location.href = '/NotFound';
+    }
+  } catch (err) {
+    throw new Error({ messge: err });
+  }
+
+  const $userData = $('.user-data');
+  const deleteConfirm = $createElement('div', 'delete-confirm');
+  deleteConfirm.innerHTML = `
 <div class="delete-confirm-wrapper">
   <h3 class="title is-3">회원 탈퇴</h3>
   <p>탈퇴를 진행하면 서비스를 이용할 수 없으며 계정과 데이터가 삭제됩니다.</p>
   <button class="button is-danger" id="userDelete">확인</button>
 </div>
 `;
-$userData.append(deleteConfirm);
+  $userData.append(deleteConfirm);
 
-function deleteUser() {
-  const { token } = JSON.parse(getCookie('userToken'));
+  function deleteUser() {
+    const { token } = JSON.parse(getCookie('userToken'));
 
-  if (confirm('정말 탈퇴하시겠습니까?')) {
-    fetch('/api/v1/users/me', {
-      method: 'DELETE',
-      headers: {
-        token,
-      },
-    })
-      .then((response) => response.json())
-      .then(() => {
-        deleteCookie('userToken');
-        alert('탈퇴가 완료되었습니다.');
-        window.location.href = '/';
+    if (confirm('정말 탈퇴하시겠습니까?')) {
+      fetch('/api/v1/users/me', {
+        method: 'DELETE',
+        headers: {
+          token,
+        },
       })
-      .catch(() => {
-        const isloginWarning = $('.login-warning');
-        if (isloginWarning === null) {
-          const loginFormWrapper = $('.login-form-wrapper');
-          const loginWarning = $createElement('div', 'login-warning');
-          loginWarning.innerText = '탈퇴를 실패하였습니다.';
-          loginFormWrapper.appendChild(loginWarning);
-        }
-      });
+        .then((response) => response.json())
+        .then(() => {
+          deleteCookie('userToken');
+          alert('탈퇴가 완료되었습니다.');
+          window.location.href = '/';
+        })
+        .catch(() => {
+          const isloginWarning = $('.login-warning');
+          if (isloginWarning === null) {
+            const loginFormWrapper = $('.login-form-wrapper');
+            const loginWarning = $createElement('div', 'login-warning');
+            loginWarning.innerText = '탈퇴를 실패하였습니다.';
+            loginFormWrapper.appendChild(loginWarning);
+          }
+        });
+    }
   }
-}
 
-function checkPassword() {
-  if (confirm('정말 탈퇴하시겠습니까?')) {
-    deleteConfirm.remove();
+  function checkPassword() {
+    if (confirm('정말 탈퇴하시겠습니까?')) {
+      deleteConfirm.remove();
 
-    const checkPasswordForm = $createElement('form', 'check-login-form');
-    checkPasswordForm.innerHTML = `
+      const checkPasswordForm = $createElement('form', 'check-login-form');
+      checkPasswordForm.innerHTML = `
     <section class="check-login-wrapper">
       <h3 class="title is-3">비밀번호 확인</h3>
       <div class="login-form-wrapper">
@@ -63,35 +76,38 @@ function checkPassword() {
       </div>
     </section>
     `;
-    $userData.append(checkPasswordForm);
-    const checkForm = $('.check-login-form');
-    checkForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const checkPassword = $('.check-password').value;
-      const { token } = JSON.parse(getCookie('userToken'));
+      $userData.append(checkPasswordForm);
+      const checkForm = $('.check-login-form');
+      checkForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const checkPassword = $('.check-password').value;
+        const { token } = JSON.parse(getCookie('userToken'));
 
-      fetch('/api/v1/auth/check', {
-        method: 'POST',
-        headers: {
-          token,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password: checkPassword }),
-      })
-        .then((response) => response.json())
-        .then(() => deleteUser())
-        .catch(() => {
-          const isloginWarning = $('.login-warning');
-          if (isloginWarning === null) {
-            const loginFormWrapper = $('.login-form-wrapper');
-            const loginWarning = $createElement('div', 'login-warning');
-            loginWarning.innerText = '비밀번호를 확인해주세요.';
-            loginFormWrapper.appendChild(loginWarning);
-          }
-        });
-    });
+        fetch('/api/v1/auth/check', {
+          method: 'POST',
+          headers: {
+            token,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ password: checkPassword }),
+        })
+          .then((response) => response.json())
+          .then(() => deleteUser())
+          .catch(() => {
+            const isloginWarning = $('.login-warning');
+            if (isloginWarning === null) {
+              const loginFormWrapper = $('.login-form-wrapper');
+              const loginWarning = $createElement('div', 'login-warning');
+              loginWarning.innerText = '비밀번호를 확인해주세요.';
+              loginFormWrapper.appendChild(loginWarning);
+            }
+          });
+      });
+    }
   }
-}
 
-const deleteButton = $('#userDelete');
-deleteButton.addEventListener('click', checkPassword);
+  const deleteButton = $('#userDelete');
+  deleteButton.addEventListener('click', checkPassword);
+};
+
+render();
--- src/views/pages/UserUpdate/UserUpdate.js
+++ src/views/pages/UserUpdate/UserUpdate.js
@@ -1,9 +1,22 @@
 import { $, $createElement, getCookie } from '/js/utils.js';
 import { getUserInfo } from '/js/api/authAPI.js';
+import { isLogin } from '/js/api/authAPI.js';
 
-const $userData = $('.user-data');
-const userUpdateForm = $createElement('form', 'update-form');
-userUpdateForm.innerHTML = `
+const render = async () => {
+  let loginEmail;
+  try {
+    loginEmail = await isLogin();
+    if (!loginEmail) {
+      alert('회원 전용 페이지입니다!');
+      location.href = '/NotFound';
+    }
+  } catch (err) {
+    throw new Error({ messge: err });
+  }
+
+  const $userData = $('.user-data');
+  const userUpdateForm = $createElement('form', 'update-form');
+  userUpdateForm.innerHTML = `
   <form class="update-form">
   <h4 class="title is-4">회원 정보 수정</h4>
   <div class="field is-horizontal">
@@ -236,8 +249,8 @@ userUpdateForm.innerHTML = `
   </div>
   </form>
 `;
-const checkPasswordForm = $createElement('form', 'check-login-form');
-checkPasswordForm.innerHTML = `
+  const checkPasswordForm = $createElement('form', 'check-login-form');
+  checkPasswordForm.innerHTML = `
 <section class="check-login-wrapper">
   <h3 class="title is-3">비밀번호 확인</h3>
   <div class="login-form-wrapper">
@@ -256,238 +269,242 @@ checkPasswordForm.innerHTML = `
   </div>
 </section>
 `;
-$userData.append(checkPasswordForm);
+  $userData.append(checkPasswordForm);
 
-function showUpdateForm() {
-  checkPasswordForm.remove();
+  function showUpdateForm() {
+    checkPasswordForm.remove();
 
-  $userData.append(userUpdateForm);
+    $userData.append(userUpdateForm);
 
-  const updateForm = $('.update-form');
-  const newUserEmail = $('#email');
-  const newUserPassword = $('#password');
-  const newUserPasswordVerify = $('#passwordVerify');
-  const newUserPhoneNumber = $('#phoneNumber');
-  const findAddress = document.querySelectorAll('.address');
-  const teams = document.getElementsByName('team');
+    const updateForm = $('.update-form');
+    const newUserEmail = $('#email');
+    const newUserPassword = $('#password');
+    const newUserPasswordVerify = $('#passwordVerify');
+    const newUserPhoneNumber = $('#phoneNumber');
+    const findAddress = document.querySelectorAll('.address');
+    const teams = document.getElementsByName('team');
 
-  function checkValidation(target) {
-    const regex = {
-      email:
-        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-      password: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/,
-    };
-    if (!target.value.match(regex[target.id])) return false;
-    return true;
-  }
-
-  function isValid(event) {
-    if (!checkValidation(event.target)) {
-      event.target.classList.add('is-danger');
-      const warning = $(`.${event.target.id}-warning`);
-      warning.style.display = '';
-    } else {
-      event.target.classList.remove('is-danger');
-      const warning = $(`.${event.target.id}-warning`);
-      warning.style.display = 'none';
+    function checkValidation(target) {
+      const regex = {
+        email:
+          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+        password: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/,
+      };
+      if (!target.value.match(regex[target.id])) return false;
+      return true;
     }
-  }
 
-  function passwordVerify() {
-    const warning = $('.password-verify-warning');
-    if (
-      newUserPassword.value !== newUserPasswordVerify.value ||
-      newUserPasswordVerify.value === ''
-    ) {
-      newUserPasswordVerify.classList.add('is-danger');
-      warning.style.display = '';
-      return false;
-    }
-    newUserPasswordVerify.classList.remove('is-danger');
-    warning.style.display = 'none';
-    return true;
-  }
-
-  function checkAddress() {
-    if (findAddress[1].value) return true;
-    return false;
-  }
-
-  function userInfoComplete() {
-    if (!checkValidation(newUserEmail)) {
-      alert('이메일 형식이 올바르지 않습니다.');
-      return false;
-    }
-    if (!checkValidation(newUserPassword)) {
-      alert('비밀번호 형식이 올바르지 않습니다.');
-      return false;
-    }
-    if (!passwordVerify()) {
-      alert('비밀번호가 일치하지 않습니다.');
-      return false;
-    }
-    if (!checkAddress()) {
-      alert('주소를 입력해 주세요');
-      return false;
-    }
-    return true;
-  }
-
-  function autoHyphen() {
-    newUserPhoneNumber.value = newUserPhoneNumber.value
-      .replace(/[^0-9]/g, '')
-      .replace(/^(\d{3,4})(\d{4})$/, '$1-$2');
-  }
-
-  function getPhoneNumber() {
-    const firstNumber = $('#firstPhoneNumber');
-    return `${firstNumber.options[firstNumber.selectedIndex].text}-${
-      newUserPhoneNumber.value
-    }`;
-  }
-
-  function searchZipcode() {
-    new daum.Postcode({
-      oncomplete(data) {
-        const roadAddr = data.roadAddress;
-        document.getElementById('postCode').value = data.zonecode;
-        document.getElementById('roughAddress').value = roadAddr;
-      },
-    }).open();
-  }
-
-  function selectTeam() {
-    teams.forEach((team) =>
-      team.checked
-        ? team.parentNode.classList.add('is-info')
-        : team.parentNode.classList.remove('is-info')
-    );
-  }
-
-  function getCheerTeam() {
-    const teamID = {
-      '롯데 자이언츠': '644221ccead2ae1ca8f0c9d0',
-      'KIA 타이거즈': '644221deead2ae1ca8f0c9d2',
-      '삼성 라이온즈': '644221e4ead2ae1ca8f0c9d4',
-      'LG 트윈스': '644221ecead2ae1ca8f0c9d6',
-      '두산 베어스': '644221f4ead2ae1ca8f0c9d8',
-      '키움 히어로즈': '644221fcead2ae1ca8f0c9da',
-      'SSG 랜더스': '64422203ead2ae1ca8f0c9dc',
-      'KT 위즈': '64422209ead2ae1ca8f0c9de',
-      '한화 이글스': '64422210ead2ae1ca8f0c9e0',
-      'NC 다이노스': '64422215ead2ae1ca8f0c9e2',
-    };
-    const checkedTeam = Array.from(teams).find((team) => team.checked);
-    let selectedTeam;
-    checkedTeam === undefined
-      ? (selectedTeam = false)
-      : (selectedTeam = teamID[checkedTeam.value]);
-    return selectedTeam;
-  }
-
-  function onUpdateSubmit(e) {
-    e.preventDefault();
-    if (userInfoComplete()) {
-      const newUser = {};
-      const userInfoKey = [
-        'email',
-        'password',
-        'koreanName',
-        'phoneNumber',
-        'postCode',
-        'roughAddress',
-        'detailAddress',
-        'cheerTeam',
-      ];
-      userInfoKey.forEach((key) => {
-        const userInfo = $(`#${key}`);
-        if (key === 'phoneNumber') {
-          newUser[key] = getPhoneNumber();
-        } else if (key === 'cheerTeam') {
-          const isSelected = getCheerTeam();
-          if (isSelected) newUser[key] = isSelected;
-        } else newUser[key] = userInfo.value;
-      });
-      const { token } = JSON.parse(getCookie('userToken'));
-
-      fetch('/api/v1/users/me', {
-        method: 'PUT',
-        headers: {
-          token,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newUser),
-      })
-        .then((response) => response.json())
-        .then(() => {
-          alert('회원정보를 수정하였습니다.');
-          window.location.href = '/user/mypage';
-        })
-        .catch(() => {
-          alert('입력한 정보를 다시 확인해주세요.');
-        });
-    }
-  }
-
-  updateForm.addEventListener('submit', onUpdateSubmit);
-  newUserEmail.addEventListener('blur', isValid);
-  newUserPassword.addEventListener('blur', isValid);
-  newUserPassword.addEventListener('blur', passwordVerify);
-  newUserPasswordVerify.addEventListener('blur', passwordVerify);
-  newUserPhoneNumber.addEventListener('input', autoHyphen);
-  for (let i = 0; i < 3; i++) {
-    findAddress[i].addEventListener('click', searchZipcode);
-  }
-  teams.forEach((node) => {
-    node.addEventListener('click', selectTeam);
-  });
-}
-
-function fillCheerTeam(userData) {
-  const teamName = userData.cheerTeam.teamName;
-  const teams = document.getElementsByName('team');
-  const checkedTeam = Array.from(teams).find((team) => team.value === teamName);
-  checkedTeam.checked = 'checked';
-  checkedTeam.parentNode.classList.add('is-info');
-}
-
-function fillUserInfo(userData) {
-  $('#email').value = userData.email;
-  $('#koreanName').value = userData.koreanName;
-  $('#phoneNumber').value = userData.phoneNumber.substring(4);
-  $('#postCode').value = userData.address.postCode;
-  $('#roughAddress').value = userData.address.roughAddress;
-  $('#detailAddress').value = userData.address.detailAddress;
-  fillCheerTeam(userData);
-}
-
-const checkForm = $('.check-login-form');
-checkForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const checkPassword = $('.check-password').value;
-  const { token } = JSON.parse(getCookie('userToken'));
-
-  fetch('/api/v1/auth/check', {
-    method: 'POST',
-    headers: {
-      token,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ password: checkPassword }),
-  })
-    .then((response) => response.json())
-    .then(async () => {
-      showUpdateForm();
-      const userData = await getUserInfo();
-      fillUserInfo(userData);
-    })
-    .catch(() => {
-      const isloginWarning = $('.login-warning');
-      if (isloginWarning === null) {
-        const loginFormWrapper = $('.login-form-wrapper');
-        const loginWarning = $createElement('div', 'login-warning');
-        loginWarning.innerText = '비밀번호가 올바르지 않습니다.';
-        loginFormWrapper.appendChild(loginWarning);
+    function isValid(event) {
+      if (!checkValidation(event.target)) {
+        event.target.classList.add('is-danger');
+        const warning = $(`.${event.target.id}-warning`);
+        warning.style.display = '';
+      } else {
+        event.target.classList.remove('is-danger');
+        const warning = $(`.${event.target.id}-warning`);
+        warning.style.display = 'none';
       }
+    }
+
+    function passwordVerify() {
+      const warning = $('.password-verify-warning');
+      if (
+        newUserPassword.value !== newUserPasswordVerify.value ||
+        newUserPasswordVerify.value === ''
+      ) {
+        newUserPasswordVerify.classList.add('is-danger');
+        warning.style.display = '';
+        return false;
+      }
+      newUserPasswordVerify.classList.remove('is-danger');
+      warning.style.display = 'none';
+      return true;
+    }
+
+    function checkAddress() {
+      if (findAddress[1].value) return true;
+      return false;
+    }
+
+    function userInfoComplete() {
+      if (!checkValidation(newUserEmail)) {
+        alert('이메일 형식이 올바르지 않습니다.');
+        return false;
+      }
+      if (!checkValidation(newUserPassword)) {
+        alert('비밀번호 형식이 올바르지 않습니다.');
+        return false;
+      }
+      if (!passwordVerify()) {
+        alert('비밀번호가 일치하지 않습니다.');
+        return false;
+      }
+      if (!checkAddress()) {
+        alert('주소를 입력해 주세요');
+        return false;
+      }
+      return true;
+    }
+
+    function autoHyphen() {
+      newUserPhoneNumber.value = newUserPhoneNumber.value
+        .replace(/[^0-9]/g, '')
+        .replace(/^(\d{3,4})(\d{4})$/, '$1-$2');
+    }
+
+    function getPhoneNumber() {
+      const firstNumber = $('#firstPhoneNumber');
+      return `${firstNumber.options[firstNumber.selectedIndex].text}-${
+        newUserPhoneNumber.value
+      }`;
+    }
+
+    function searchZipcode() {
+      new daum.Postcode({
+        oncomplete(data) {
+          const roadAddr = data.roadAddress;
+          document.getElementById('postCode').value = data.zonecode;
+          document.getElementById('roughAddress').value = roadAddr;
+        },
+      }).open();
+    }
+
+    function selectTeam() {
+      teams.forEach((team) =>
+        team.checked
+          ? team.parentNode.classList.add('is-info')
+          : team.parentNode.classList.remove('is-info')
+      );
+    }
+
+    function getCheerTeam() {
+      const teamID = {
+        '롯데 자이언츠': '644221ccead2ae1ca8f0c9d0',
+        'KIA 타이거즈': '644221deead2ae1ca8f0c9d2',
+        '삼성 라이온즈': '644221e4ead2ae1ca8f0c9d4',
+        'LG 트윈스': '644221ecead2ae1ca8f0c9d6',
+        '두산 베어스': '644221f4ead2ae1ca8f0c9d8',
+        '키움 히어로즈': '644221fcead2ae1ca8f0c9da',
+        'SSG 랜더스': '64422203ead2ae1ca8f0c9dc',
+        'KT 위즈': '64422209ead2ae1ca8f0c9de',
+        '한화 이글스': '64422210ead2ae1ca8f0c9e0',
+        'NC 다이노스': '64422215ead2ae1ca8f0c9e2',
+      };
+      const checkedTeam = Array.from(teams).find((team) => team.checked);
+      let selectedTeam;
+      checkedTeam === undefined
+        ? (selectedTeam = false)
+        : (selectedTeam = teamID[checkedTeam.value]);
+      return selectedTeam;
+    }
+
+    function onUpdateSubmit(e) {
+      e.preventDefault();
+      if (userInfoComplete()) {
+        const newUser = {};
+        const userInfoKey = [
+          'email',
+          'password',
+          'koreanName',
+          'phoneNumber',
+          'postCode',
+          'roughAddress',
+          'detailAddress',
+          'cheerTeam',
+        ];
+        userInfoKey.forEach((key) => {
+          const userInfo = $(`#${key}`);
+          if (key === 'phoneNumber') {
+            newUser[key] = getPhoneNumber();
+          } else if (key === 'cheerTeam') {
+            const isSelected = getCheerTeam();
+            if (isSelected) newUser[key] = isSelected;
+          } else newUser[key] = userInfo.value;
+        });
+        const { token } = JSON.parse(getCookie('userToken'));
+
+        fetch('/api/v1/users/me', {
+          method: 'PUT',
+          headers: {
+            token,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newUser),
+        })
+          .then((response) => response.json())
+          .then(() => {
+            alert('회원정보를 수정하였습니다.');
+            window.location.href = '/user/mypage';
+          })
+          .catch(() => {
+            alert('입력한 정보를 다시 확인해주세요.');
+          });
+      }
+    }
+
+    updateForm.addEventListener('submit', onUpdateSubmit);
+    newUserEmail.addEventListener('blur', isValid);
+    newUserPassword.addEventListener('blur', isValid);
+    newUserPassword.addEventListener('blur', passwordVerify);
+    newUserPasswordVerify.addEventListener('blur', passwordVerify);
+    newUserPhoneNumber.addEventListener('input', autoHyphen);
+    for (let i = 0; i < 3; i++) {
+      findAddress[i].addEventListener('click', searchZipcode);
+    }
+    teams.forEach((node) => {
+      node.addEventListener('click', selectTeam);
     });
-});
+  }
+
+  function fillCheerTeam(userData) {
+    const teamName = userData.cheerTeam.teamName;
+    const teams = document.getElementsByName('team');
+    const checkedTeam = Array.from(teams).find(
+      (team) => team.value === teamName
+    );
+    checkedTeam.checked = 'checked';
+    checkedTeam.parentNode.classList.add('is-info');
+  }
+
+  function fillUserInfo(userData) {
+    $('#email').value = userData.email;
+    $('#koreanName').value = userData.koreanName;
+    $('#phoneNumber').value = userData.phoneNumber.substring(4);
+    $('#postCode').value = userData.address.postCode;
+    $('#roughAddress').value = userData.address.roughAddress;
+    $('#detailAddress').value = userData.address.detailAddress;
+    fillCheerTeam(userData);
+  }
+
+  const checkForm = $('.check-login-form');
+  checkForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const checkPassword = $('.check-password').value;
+    const { token } = JSON.parse(getCookie('userToken'));
+
+    fetch('/api/v1/auth/check', {
+      method: 'POST',
+      headers: {
+        token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password: checkPassword }),
+    })
+      .then((response) => response.json())
+      .then(async () => {
+        showUpdateForm();
+        const userData = await getUserInfo();
+        fillUserInfo(userData);
+      })
+      .catch(() => {
+        const isloginWarning = $('.login-warning');
+        if (isloginWarning === null) {
+          const loginFormWrapper = $('.login-form-wrapper');
+          const loginWarning = $createElement('div', 'login-warning');
+          loginWarning.innerText = '비밀번호가 올바르지 않습니다.';
+          loginFormWrapper.appendChild(loginWarning);
+        }
+      });
+  });
+};
+render();
