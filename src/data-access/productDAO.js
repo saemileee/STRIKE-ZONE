2791b3ff10e9d98d14877fd97669a396c00891cc
[SW4/2반] 표후동 <vvhooping@gmail.com>
2023-04-24 23:36:30 +0900

    refactor: productId, orderId 를 구하는 코드 수정
    - 수정 전 : 전체 documents 를 다 조회후 sort 및 limit(1) 한 후
      last document 의 id 를 꺼내서 + 1 처리 후 return
    
    - 수정 후 : 전체 documents count + 1 하여 return

--- src/data-access/productDAO.js
+++ src/data-access/productDAO.js
@@ -43,12 +43,8 @@ const productDAO = {
   },
 
   async createProductId() {
-    // 가장 마지막에 등록한 상품을 검색해서 해당 productId 에서 +1 을 한 값을
-    // 새로 등록할 상품의 productId 로 사용한다.
-    const lastProduct = await Product.find({}).sort({ createdAt: -1 }).limit(1);
-    const lastProductId = lastProduct.length === 0 ? 0 : lastProduct[0].productId;
-    const nextProductId = lastProductId + 1;
-
+    const currentDocumentsCount = await Product.countDocuments();
+    const nextProductId = currentDocumentsCount === 0 ? 1 : currentDocumentsCount + 1;
     return nextProductId;
   },
 };
