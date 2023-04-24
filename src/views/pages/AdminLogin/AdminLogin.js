0dea5b3d19723ddc558cb0d845e744aa42071c93
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-04-24 17:01:37 +0900

    feat: 관리자 로그인 페이지 구현

---
+++ src/views/pages/AdminLogin/AdminLogin.js
@@ -0,0 +1,53 @@
+import { $, $createElement } from '/js/utils.js';
+import { isAdmin } from '/js/api/authAPI.js';
+
+const loginForm = $('.login-form');
+
+const loginId = $('#loginId');
+const loginPassword = $('#loginPassword');
+
+const onLoginSubmit = (e) => {
+  e.preventDefault();
+  const adminId = loginId.value;
+  const adminPassword = loginPassword.value;
+  const adminInfo = { email: adminId, password: adminPassword };
+
+  fetch('/api/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(adminInfo),
+  })
+    .then((response) => response.json())
+    .then(async (token) => {
+      const adminToken = JSON.stringify(token);
+      document.cookie = `userToken=${adminToken}; path=/`;
+
+      let tokenIsAdmin;
+      try {
+        tokenIsAdmin = await isAdmin();
+        console.log(tokenIsAdmin);
+        if (!tokenIsAdmin) {
+          alert('관리자 전용 페이지입니다!');
+          location.href = '/login';
+        } else {
+          alert('관리자 로그인 성공');
+          window.location.href = '/user/mypage';
+        }
+      } catch (err) {
+        throw new Error({ messge: err });
+      }
+    })
+    .catch(() => {
+      const isloginWarning = $('.login-warning');
+      if (isloginWarning === null) {
+        const loginFormWrapper = $('.login-form-wrapper');
+        const loginWarning = $createElement('div', 'login-warning');
+        loginWarning.innerText = '아이디 또는 비밀번호가 올바르지 않습니다.';
+        loginFormWrapper.appendChild(loginWarning);
+      }
+    });
+};
+
+loginForm.addEventListener('submit', onLoginSubmit);
