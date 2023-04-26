68b4ca46002b34d66c21bc18737afeae7a0de3b5
Saem <saemilee01@gmail.com>
2023-04-26 19:51:03 +0900

    fix : footer 내용 수정 및 링크 연결

--- src/public/js/footer.js
+++ src/public/js/footer.js
@@ -1,4 +1,7 @@
 const footerElement = document.createElement('footer');
-footerElement.innerHTML = `<div class="footer-wrapper"><div><h2>STRIKE ZONE</h2></div><ul><div>저자 /</div><li><a href="#">김민규</a></li><li><a href="#">이선우</a></li><li><a href="#">이새미</a></li><li><a href="#">이지원</a></li><li><a href="#">표후동</a></li></ul><p>© 2023. Elice SW4 Track. homerunball Team. all rights reserved.</p></div>`;
+footerElement.innerHTML = `<div class="footer-wrapper"><div><h2>STRIKE ZONE</h2></div><ul><div>저자 /</div><li><a href="https://github.com/Cobocho" target="_blank">김민규</a></li><li><a href="https://github.com/LL-SS
+" target="_blank">이선우</a></li><li><a href="https://github.com/saemileee" target="_blank">이새미</a></li><li><a href="https://github.com/LEEJW1953" target="_blank">이지원</a></li><li><a href="https://github.com/hdpyo" target="_blank">표후동</a></li></ul>
+<p>본 사이트는 엘리스 SW트랙 4기에서 진행한 웹사이트 구축 프로젝트 용 사이트입니다.</p>
+<p>© 2023. Elice SW4 Track. homerunball Team. all rights reserved.</p></div>`;
 
 document.body.append(footerElement);
