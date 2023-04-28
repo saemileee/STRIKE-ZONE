68074f9e0edef134336616f191b97ff249f4daf4
hdpyo <vvhooping@gmail.com>
2023-04-28 14:55:41 +0900

    refactor: 사용하지 않는 코드 제거

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,9 +1,5 @@
 import { orderDAO, productDAO, shippingDAO } from '../data-access/model';
 
-const DEFAULT_PAYMENT_METHOD = '무통장 입금';
-const STATUS_BY_DEFAULT_PAYMENT_METHOD = '결제 전';
-const STATUS_BY_PAYMENT_METHOD = '상품 준비중';
-
 const orderService = {
 
   // 주문 저장하기
@@ -111,18 +107,6 @@ const orderService = {
     await Promise.all(promises);
 
     return { deletedCount, modifiedCount };
-  },
-
-  // 결제 수단에 따라 기본 배송 상태를 정하기 ('결제전' or '상품준비중')
-  setOrderStatusByPaymentMethod(paymentMethod) {
-    let status = STATUS_BY_PAYMENT_METHOD;
-
-    // TODO: 프론트단에서도 미리 결제 수단이 undefined 로 넘어오지 않도록 처리하는 게 더 좋을 것 같다.
-    if (paymentMethod === undefined || paymentMethod === DEFAULT_PAYMENT_METHOD) {
-      status = STATUS_BY_DEFAULT_PAYMENT_METHOD;
-    }
-
-    return status;
   },
 
   async calculatePayments(orderInfo) {
